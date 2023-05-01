7ab2c968c845281b79c37e01b7fc6824b7525217
Ryan <119005046+CodeByDex@users.noreply.github.com>
2023-05-01 17:51:48 -0400

    put options in connect

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const Sequelize = require("sequelize");
 
 const db = process.env.JAWSDB_URL
-    ? new Sequelize(process.env.JAWSDB_URL, {})
+    ? new Sequelize(process.env.JAWSDB_URL)
     : new Sequelize(process.env.DB_Name, 
         process.env.DB_User,
         process.env.DB_PW,
