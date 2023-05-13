f105d389252576fe5a25b613634bab18d84f0cff
Ryan <119005046+CodeByDex@users.noreply.github.com>
2023-05-13 09:15:16 -0400

    initial tailwind setup

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const routes = require("./controllers");
 const exphbs = require("express-handlebars");
 const sequelize = require("./config/connection");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,7 +13,9 @@ app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
+
 
 app.use(routes);
 
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.handlebars", 
+              "./public/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
