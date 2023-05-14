5a78af4d00ebdec28fbc836be0736087904bb7f3
Ryan <119005046+CodeByDex@users.noreply.github.com>
2023-05-14 09:54:45 -0400

    added logout redirect

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -1,5 +1,18 @@
 window.addEventListener("load", (event) => {
     document.querySelector("#Logout").addEventListener("submit", async (event) => {
-        location.replace("/login");
+        event.preventDefault();
+
+        const response = await fetch('/api/user/logout', {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            }
+        })
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert("Error Logging Out");
+        }
     })
 });
